ae02c56095ba0a00841159ed5d5092ee2787217e
Silvano Stralla <silvano.stralla@sistrall.it>
2023-03-10 15:14:33 +0100

    Add test that includes defaultMetaTransformer for links

--- src/lib/components/__tests__/__fixtures__/ItemLink.svelte
+++ src/lib/components/__tests__/__fixtures__/ItemLink.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
+	import { defaultMetaTransformer } from 'datocms-structured-text-generic-html-renderer';
 	import { isItemLink, type ItemLink } from 'datocms-structured-text-utils';
 	import type { DocPageRecord } from './types';
 
 	export let node: ItemLink;
 	export let link: DocPageRecord;
 
-	// FIXME
-	const transformedMeta = {};
+	$: ({ meta } = node)
+	$: transformedMeta = meta ? defaultMetaTransformer({node, meta}) : null;
 </script>
 
 {#if link.__typename == 'DocPageRecord' && isItemLink(node)}
