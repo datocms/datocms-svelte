f9f56b45f0bd37aa0a6a22bb21c57e04aaaea845
Silvano Stralla <silvano.stralla@sistrall.it>
2023-03-10 13:27:56 +0100

    Rework library entry point

--- src/lib/components/StructuredText.svelte
+++ src/lib/components/StructuredText.svelte
@@ -1,18 +1,13 @@
-<script context="module" lang="ts">
-	export type { StructuredText, Document } from 'datocms-structured-text-utils';
-</script>
-
 <script lang="ts">
-	import type { SvelteComponentTyped } from 'svelte';
-	import type { StructuredText, Node as N } from 'datocms-structured-text-utils';
+	import type { StructuredText } from 'datocms-structured-text-utils';
 
+	import type { PredicateComponentTuple } from '$lib';
+	
 	import Node from './_StructuredText/Node.svelte';
 
 	/** The actual field value you get from DatoCMS **/
 	// FIXME: add support for documents (from CMA) and node (for rendering branches)
 	export let data: StructuredText | null = null;
-
-	type PredicateComponentTuple = [(n: N) => boolean, new (...any: any[]) => SvelteComponentTyped];
 
 	export let components: PredicateComponentTuple[] = [];
 
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,10 @@
-export * as StructuredText from "./components/StructuredText.svelte";
-export * as Image from "./components/Image.svelte";
+import type { Node } from 'datocms-structured-text-utils';
+import type { SvelteComponentTyped } from 'svelte';
+
+export { default as StructuredText } from './components/StructuredText.svelte';
+export { default as Image } from './components/Image.svelte';
+
+export type PredicateComponentTuple = [
+	(n: Node) => boolean,
+	new (...any: any) => SvelteComponentTyped
+];
