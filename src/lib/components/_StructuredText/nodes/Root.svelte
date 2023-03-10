c812c9576d87c66a33d76921ed06f6d1395d4dcb
Silvano Stralla <silvano.stralla@sistrall.it>
2023-03-10 13:28:18 +0100

    Explain the unused prop hack

--- src/lib/components/_StructuredText/nodes/Root.svelte
+++ src/lib/components/_StructuredText/nodes/Root.svelte
@@ -2,6 +2,20 @@
 	import type { Root } from 'datocms-structured-text-utils';
 
 	export let node: Root;
+
+	node;
+	// ↑
+	// This is a hack: it's important to declare the `node` prop: all the components
+	// for rendering node types will receive the node at rendering time. That gives
+	// users of the library to write custom components with special behaviours based on the
+	// node specifities. But we don't use the prop in this default block, so the linter
+	// triggers a warning. At the same time, in Svelte there so reliable way to disable the
+  // `svelte(unused-export-let)` warning.
+  // 
+  // Mentioning the node like this will silence the warning.
+  // 
+  // If you're building a custom component and you're gonna use the node in the component,
+  // you can omit this statement.
 </script>
 
 <slot />
