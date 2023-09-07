5526b80c1c76aca40864b34b2742a50c22849d10
Juerg Hunziker <juerg.hunziker@liip.ch>
2023-09-07 10:43:05 +0200

    add link attributes from meta field

--- src/lib/components/StructuredText/nodes/Link.svelte
+++ src/lib/components/StructuredText/nodes/Link.svelte
@@ -3,7 +3,22 @@
 
 	export let node: Link;
 
-	$: ({ url } = node);
+	$: ({ url, meta } = node);
+
+	let target: string | undefined = undefined;
+	$: {
+		const targetMetaEntry =  meta?.find((metaEntry) => metaEntry.id === 'target');
+		if (targetMetaEntry?.value) {
+			target = targetMetaEntry.value;
+		}
+	}
+	let rel: string | undefined = undefined;
+	$: {
+		const relMetaEntry =  meta?.find((metaEntry) => metaEntry.id === 'rel');
+		if (relMetaEntry?.value) {
+			rel = relMetaEntry.value;
+		}
+	}
 </script>
 
-<a href={url}><slot /></a>
+<a href={url} {target} {rel}><slot /></a>
